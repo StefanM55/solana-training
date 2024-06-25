b64fde0cfdcbb1d5ce6506b154e790fc1ede2379
stefanM55 <stefanmiloiu@live.com>
2024-06-25 19:57:37 +0300

    add 1st workshop 15 june

---
+++ generate-keypair.ts
@@ -0,0 +1,23 @@
+// 1st
+
+// import { Keypair } from "@solana/web3.js";
+// const keypair = Keypair.generate();
+// console.log(`✅ Generated keypair!`)
+
+// 2nd
+
+// import { Keypair } from "@solana/web3.js";
+
+// const { publicKey, secretKey} = Keypair.generate();
+
+// console.log(`The public key is: `, publicKey.toBase58());
+// console.log(`The secret key is: `, secretKey);
+// console.log(`✅ Finished!`);
+
+// 3rd
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(`✅ Finished! We've loaded our keypair securely, using an env file! Our public key is: ${keypair.publicKey.toBase58()}`);
