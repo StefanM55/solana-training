8962fadce0fa89d9095ee59fef69f20c1e9ffd08
stefanM55 <stefanmiloiu@live.com>
2024-07-30 19:28:54 +0300

    make tx from dapp in workshop

--- workshop30July/pages/index.tsx
+++ workshop30July/pages/index.tsx
@@ -3,6 +3,7 @@ import styles from "../styles/Home.module.css";
 import { AppBar } from "../components/AppBar";
 import Head from "next/head";
 import { PingButton } from "../components/PingButton";
+import { WalletContextProvider } from "../components/WalletContextProvider";
 
 const Home: NextPage = (props) => {
   return (
@@ -11,13 +12,15 @@ const Home: NextPage = (props) => {
         <title>Wallet-Adapter Example</title>
         <meta name="description" content="Wallet-Adapter Example" />
       </Head>
-      <AppBar />
-      <div className={styles.AppBody}>
-        <PingButton />
-      </div>
+
+      <WalletContextProvider>
+        <AppBar />
+        <div className={styles.AppBody}>
+          <PingButton />
+        </div>
+      </WalletContextProvider>
     </div>
   );
 };
 
 export default Home;
-
