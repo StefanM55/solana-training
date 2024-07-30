2974de7b460a450c68b2aa0071eb35f462f064c5
stefanM55 <stefanmiloiu@live.com>
2024-07-30 19:45:14 +0300

    add bonus to workshop

--- workshop30July/pages/index.tsx
+++ workshop30July/pages/index.tsx
@@ -4,8 +4,15 @@ import { AppBar } from "../components/AppBar";
 import Head from "next/head";
 import { PingButton } from "../components/PingButton";
 import { WalletContextProvider } from "../components/WalletContextProvider";
+import { useEffect, useState } from "react";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 
 const Home: NextPage = (props) => {
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+
+  const [solAmount, setSolAmount] = useState(0);
+  const [recipientAddress, setRecipientAddress] = useState("");
   return (
     <div className={styles.App}>
       <Head>
@@ -15,8 +22,13 @@ const Home: NextPage = (props) => {
 
       <WalletContextProvider>
         <AppBar />
+        <p>AMOUNT</p>
+        <input type="number" value={solAmount} onChange={(e) => setSolAmount(parseFloat(e.target.value))} placeholder="Amount in SOL" />
+
+        <p>RECIPIENT ADDRESS</p>
+        <input type="text" value={recipientAddress} onChange={(e) => setRecipientAddress(e.target.value)} placeholder="Recipient Address" />
         <div className={styles.AppBody}>
-          <PingButton />
+          <PingButton recipientAddress={recipientAddress} solAmount={solAmount} />
         </div>
       </WalletContextProvider>
     </div>
